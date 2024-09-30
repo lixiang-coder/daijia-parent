c06c1cc7f3b0d182ab358d8de68aa49fe48894e5
xzy <2894702202@qq.com>
2024-09-30 15:43:15 +0800

    feat：司机更新代驾车辆信息

--- service/service-order/src/main/java/com/atguigu/daijia/order/controller/OrderInfoController.java
+++ service/service-order/src/main/java/com/atguigu/daijia/order/controller/OrderInfoController.java
@@ -3,7 +3,6 @@ package com.atguigu.daijia.order.controller;
 import com.atguigu.daijia.common.result.Result;
 import com.atguigu.daijia.model.entity.order.OrderInfo;
 import com.atguigu.daijia.model.form.order.OrderInfoForm;
-import com.atguigu.daijia.model.form.order.UpdateOrderCartForm;
 import com.atguigu.daijia.model.vo.order.CurrentOrderInfoVo;
 import com.atguigu.daijia.order.service.OrderInfoService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -60,12 +59,6 @@ public class OrderInfoController {
     @GetMapping("/driverArriveStartLocation/{orderId}/{driverId}")
     public Result<Boolean> driverArriveStartLocation(@PathVariable Long orderId, @PathVariable Long driverId) {
         return Result.ok(orderInfoService.driverArriveStartLocation(orderId, driverId));
-    }
-
-    @Operation(summary = "更新代驾车辆信息")
-    @PostMapping("/updateOrderCart")
-    public Result<Boolean> updateOrderCart(@RequestBody UpdateOrderCartForm updateOrderCartForm) {
-        return Result.ok(orderInfoService.updateOrderCart(updateOrderCartForm));
     }
 
 }
