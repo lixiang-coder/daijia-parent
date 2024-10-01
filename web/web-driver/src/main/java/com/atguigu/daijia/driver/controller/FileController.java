df653e8fb1fc29501fdc29741460c157685cfd9b
xzy <2894702202@qq.com>
2024-10-01 17:10:25 +0800

    fix：Minio上传接口

--- web/web-driver/src/main/java/com/atguigu/daijia/driver/controller/FileController.java
+++ web/web-driver/src/main/java/com/atguigu/daijia/driver/controller/FileController.java
@@ -1,27 +1,38 @@
 package com.atguigu.daijia.driver.controller;
 
-import com.atguigu.daijia.common.checklogin.XZYLogin;
 import com.atguigu.daijia.common.result.Result;
-import com.atguigu.daijia.driver.service.CosService;
-import com.atguigu.daijia.model.vo.driver.CosUploadVo;
+import com.atguigu.daijia.driver.service.FileService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "上传管理接口")
 @RestController
 @RequestMapping("file")
 public class FileController {
-    @Resource
-    private CosService cosService;
+    /*@Resource
+    private CosService cosService;*/
 
-    @Operation(summary = "文件上传")
+    @Resource
+    private FileService fileService;
+
+    // 接口冲突：上次的测式司机到达后录入车辆信息 前端调用的就是 filecontroller 的upload接口 所以司机的前后车照都放minio了
+    /*@Operation(summary = "文件上传")
     @XZYLogin
     @PostMapping("/upload")
     public Result<CosUploadVo> upload(@RequestPart("file") MultipartFile file, @RequestParam(name = "path", defaultValue = "auth") String path) {
         return Result.ok(cosService.upload(file, path));
+    }*/
+
+    @Operation(summary = "Minio文件上传")
+    @PostMapping("upload")
+    public Result<String> upload(@RequestPart("file") MultipartFile file) {
+        return Result.ok(fileService.upload(file));
     }
 
 }
